a93c6eaa35bd839bc5e9e2603ce333fab6fa1c04
Furqat Abduvasikov <f.abduvasikov@dataware.uz>
2025-08-17 19:04:19 +0500

    fix : entity folder

--- Entitys/Git.cs
+++
@@ -1,9 +0,0 @@
-namespace Git.Example.Entity
-;
-
-public class Git
-{
-    public int Id { get; set; }
-    public double Version { get; set; }
-    public string Massage { get; set; }
-}
---
+++ Git_Example/Entities/Git.cs
@@ -0,0 +1,10 @@
+namespace Git_Example.Entities ;
+
+public class Git
+{
+    public int Id { get; set; }
+    
+    public double Version { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+}
