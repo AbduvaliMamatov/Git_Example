7ccfb48577a0a30399cddc6a9429feecb11462b3
Elyor Xatanboyev <elxatamboyev2005@gmail.com>
2025-08-17 20:35:35 +0500

    feat: register repository, controller, dbCOntext

--- Git_Example/Program.cs
+++ Git_Example/Program.cs
@@ -1,3 +1,15 @@
+using Git_Example;
+using Git_Example.Data;
+using Git_Example.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<GitContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddControllers();
+builder.Services.AddScoped<IGitRepository, GitRepository>();
+
 var app = builder.Build();
 app.Run();
