4cc6b2997202b51f1801022fac1d7316586c3f43
Elyor Xatanboyev <elxatamboyev2005@gmail.com>
2025-08-17 19:07:38 +0500

    feat: add T into TKey

---
+++ Git_Example/BaseRepository.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Git_Example;
+
+public class BaseRepository<TContext, TEntity, TKey>(TContext context, TEntity entity, TKey id) : IBaseRepository<TEntity, TKey>
+    where TEntity : class
+    where TContext : DbContext
+    where TKey : IComparable, IEquatable<TKey>
+{
+    public Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
