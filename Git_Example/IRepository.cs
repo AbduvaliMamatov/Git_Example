eecae5e4c20e1ffd082469576ac34ae646e4bf3d
Elyor Xatanboyev <elxatamboyev2005@gmail.com>
2025-08-17 18:48:05 +0500

    feat: rename repository

--- Git_Example/IRepository.cs
+++ Git_Example/IRepository.cs
@@ -2,6 +2,9 @@ namespace Git_Example;
 
 public interface IBaseRepository<T> where T : class
 {
-    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(T id, CancellationToken cancellationToken = default);
     Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
 }
--- Git_Example/Repository.cs
+++ Git_Example/Repository.cs
@@ -2,16 +2,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Git_Example;
 
-public class Repository<TContext,T>(TContext context, T) : IBaseRepository<T>
+public class BaseRepository<TContext,T>(TContext context, T entity) : IBaseRepository<T>
     where T : class
     where TContext: DbContext
 {
+    public Task<T> AddAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
     public Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
-    public Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    public Task<T?> GetByIdAsync(T id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
